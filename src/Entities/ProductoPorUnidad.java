f360145165ad8e218a33494403c0aefcbd6b4516
adriandlca <adrian.dlcruz.a@gmail.com>
2025-08-07 02:00:56 -0500

    trigger

--- src/Entities/ProductoPorUnidad.java
+++ src/Entities/ProductoPorUnidad.java
@@ -1,6 +1,7 @@
 package Entities;
 
 public class ProductoPorUnidad extends Producto{
+
     public ProductoPorUnidad(String productName, int amount, double widthMeters, double heightMeters, double priceBuyxMeters, double priceSellxMeters) {
         super(productName, amount, widthMeters, heightMeters, priceBuyxMeters, priceSellxMeters);
     }
